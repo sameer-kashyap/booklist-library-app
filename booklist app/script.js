195316df74b87097e392d353e830d71dbd5811b6
Sameer Kashyap <kashyapsameer731@gmail.com>
2026-02-14 08:02:28 +0530

    Add files via upload

---
+++ booklist app/script.js
@@ -0,0 +1,27 @@
+class book{
+    constructor(title,author,isbn){
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+    }
+}
+
+class ui{
+    static displaybooks(){
+        const storebooks=[
+            {
+            title:'Book One',
+            author:'John Doe',
+            isbn:'3434434'
+        },
+        {
+            title:'Book Two',
+            author:'John Doe',
+            isbn:'45545'
+        }
+
+        
+    ];
+    const books=storebooks;
+    }
+}
